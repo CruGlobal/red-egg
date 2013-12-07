c708d349b900a3942604fc7cb87468ae7225295f
Matthew Drees <matt.drees@cru.org>
2013-12-06 18:05:18 -0700

    need to pass the errorqueue

--- src/main/java/org/cru/redegg/recording/cdi/CdiRecorderFactory.java
+++ src/main/java/org/cru/redegg/recording/cdi/CdiRecorderFactory.java
@@ -1,5 +1,6 @@
 package org.cru.redegg.recording.cdi;
 
+import org.cru.redegg.reporting.ErrorQueue;
 import org.cru.redegg.recording.api.ErrorRecorder;
 import org.cru.redegg.recording.api.RecorderFactory;
 import org.cru.redegg.recording.api.WebErrorRecorder;
@@ -7,7 +8,6 @@ import org.cru.redegg.recording.impl.DefaultErrorRecorder;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 
@@ -23,11 +23,14 @@ public class CdiRecorderFactory implements RecorderFactory {
     @Inject
     WebErrorRecorder webErrorRecorder;
 
+    @Inject
+    ErrorQueue errorQueue;
+
     @Override
     public ErrorRecorder getRecorder() {
         if (beanManager.getContext(RequestScoped.class).isActive())
             return webErrorRecorder;
         else
-            return new DefaultErrorRecorder();
+            return new DefaultErrorRecorder(errorQueue);
     }
 }
