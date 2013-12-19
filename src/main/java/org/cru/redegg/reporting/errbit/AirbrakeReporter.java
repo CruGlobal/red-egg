4c66dc0289892083c07d58cf6467d3172620278f
Matthew Drees <matt.drees@cru.org>
2013-12-19 10:33:54 -0700

    document what this is and why it’s not being used

--- src/main/java/org/cru/redegg/reporting/errbit/AirbrakeReporter.java
+++ src/main/java/org/cru/redegg/reporting/errbit/AirbrakeReporter.java
@@ -10,6 +10,22 @@ import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
 
 /**
+ * A reporter that uses Airbrake's java client:
+ * https://github.com/airbrake/airbrake-java
+ *
+ * To use this instead of NativeErrbitReporter, add to your alternatives list in beans.xml:
+ *
+ * <pre>{@code
+ * <alternatives>
+ *     <class>org.cru.redegg.reporting.errbit.AirbrakeReporter</class>
+ * </alternatives>
+ * }</pre>
+ *
+ * Note: this hasn't been tested with the real Airbrake service, or with Errbit for that matter.
+ * It was my original plan to use this for reporting errors to Errbit,
+ * but I eventually wrote NativeErrbitReporter
+ * to take advantage of some of the features of errbit (user context, github linking).
+ *
  * @author Matt Drees
  */
 @Alternative
