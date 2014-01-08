b7751827021de38fc0b73134c124ed0e9baaf452
Matthew Drees <matt.drees@cru.org>
2014-01-08 11:37:02 -0700

    use correct Level class

--- src/test/java/org/cru/redegg/recording/impl/DefaultErrorRecorderTest.java
+++ src/test/java/org/cru/redegg/recording/impl/DefaultErrorRecorderTest.java
@@ -4,13 +4,13 @@ import org.cru.redegg.recording.api.Serializer;
 import org.cru.redegg.reporting.ErrorReport;
 import org.cru.redegg.reporting.api.ErrorQueue;
 import org.hamcrest.CoreMatchers;
-import org.jboss.logmanager.Level;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -55,7 +55,7 @@ public class DefaultErrorRecorderTest
     @Test
     public void testReportWithOneLoggedException() throws Exception
     {
-        LogRecord record = new LogRecord(Level.ERROR, "bad stuff");
+        LogRecord record = new LogRecord(Level.SEVERE, "bad stuff");
         record.setThrown(root);
         recorder.recordLogRecord(record);
         ErrorReport report = recorder.buildReport();
@@ -67,11 +67,11 @@ public class DefaultErrorRecorderTest
     @Test
     public void testReportWithTwoUnrelatedLoggedExceptions() throws Exception
     {
-        LogRecord record1 = new LogRecord(Level.ERROR, "bad stuff");
+        LogRecord record1 = new LogRecord(Level.SEVERE, "bad stuff");
         record1.setThrown(root);
         recorder.recordLogRecord(record1);
 
-        LogRecord record2 = new LogRecord(Level.ERROR, "more bad stuff");
+        LogRecord record2 = new LogRecord(Level.SEVERE, "more bad stuff");
         record2.setThrown(unrelated);
         recorder.recordLogRecord(record2);
 
@@ -87,11 +87,11 @@ public class DefaultErrorRecorderTest
     @Test
     public void testReportWithTwoRelatedLoggedExceptions() throws Exception
     {
-        LogRecord record1 = new LogRecord(Level.ERROR, "bad stuff");
+        LogRecord record1 = new LogRecord(Level.SEVERE, "bad stuff");
         record1.setThrown(root);
         recorder.recordLogRecord(record1);
 
-        LogRecord record2 = new LogRecord(Level.ERROR, "wrapping the bad stuff");
+        LogRecord record2 = new LogRecord(Level.SEVERE, "wrapping the bad stuff");
         record2.setThrown(wrapper);
         recorder.recordLogRecord(record2);
 
@@ -106,7 +106,7 @@ public class DefaultErrorRecorderTest
     @Test
     public void testReportWithALoggedExceptionAndThenARecordedException() throws Exception
     {
-        LogRecord record = new LogRecord(Level.ERROR, "bad stuff");
+        LogRecord record = new LogRecord(Level.SEVERE, "bad stuff");
         record.setThrown(root);
         recorder.recordLogRecord(record);
 
@@ -123,7 +123,7 @@ public class DefaultErrorRecorderTest
     {
         recorder.recordThrown(unrelated);
 
-        LogRecord record = new LogRecord(Level.ERROR, "bad stuff");
+        LogRecord record = new LogRecord(Level.SEVERE, "bad stuff");
         record.setThrown(root);
         recorder.recordLogRecord(record);
 
